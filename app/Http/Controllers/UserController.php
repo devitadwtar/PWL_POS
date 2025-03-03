c0d8d7efcfdd7daa9fc6cccc9b1d6f8126c051ce
Devita Dwi Lestari <devitadwilestari2@gmail.com>
2025-03-03 12:39:02 +0700

    Praktikum-2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,22 +6,15 @@ use Illuminate\Http\Request;
 use App\Models\UserModel; 
 use Illuminate\Support\Facades\Hash; 
 
+
 class UserController extends Controller 
 { 
     public function index() 
-    { 
-        $data = [ 
-            'level_id' => 2, 
-            'username' => 'manager_tiga', 
-            'nama' => 'Manager 3', 
-            'password' => Hash::make('12345') 
-        ]; 
-
-        // Simpan data ke database
-        UserModel::create($data); 
-
+    {  
         // Ambil semua data user
-        $user = UserModel::all(); 
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        }); 
 
         // Tampilkan ke view
         return view('user', ['data' => $user]); 
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,27 +5,19 @@
 </head> 
 <body> 
     <h1>Data User</h1> 
-
-    @if(count($data) > 0)
-        <table border="1" cellpadding="2" cellspacing="0"> 
-            <tr> 
-                <th>ID</th> 
-                <th>Username</th> 
-                <th>Nama</th> 
-                <th>ID Level Pengguna</th> 
-            </tr> 
-            @foreach ($data as $d) 
-            <tr> 
-                <td>{{ $d->user_id }}</td> 
-                <td>{{ $d->username }}</td> 
-                <td>{{ $d->nama }}</td> 
-                <td>{{ $d->level_id }}</td> 
-            </tr> 
-            @endforeach 
-        </table> 
-    @else
-        <p>Tidak ada data user.</p>
-    @endif
-
+    <table border="1" cellpadding="2" cellspacing="0"> 
+        <tr> 
+            <th>ID</th> 
+            <th>Username</th> 
+            <th>Nama</th> 
+            <th>ID Level Pengguna</th> 
+        </tr> 
+        <tr> 
+            <td>{{ $data->user_id }}</td> 
+            <td>{{ $data->username }}</td> 
+            <td>{{ $data->nama }}</td> 
+            <td>{{ $data->level_id }}</td> 
+        </tr> 
+    </table> 
 </body> 
 </html>
