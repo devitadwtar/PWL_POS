3a5ff6e7a1243a9a6ff1fe2cbc85994b3971dc26
Devita Dwi Lestari <devitadwilestari2@gmail.com>
2025-03-07 22:27:20 +0700

    Praktikum-2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,14 +6,12 @@ use Illuminate\Http\Request;
 use App\Models\UserModel; 
 use Illuminate\Support\Facades\Hash; 
 
-
 class UserController extends Controller 
 { 
     public function index() 
     {  
-
-        // Ambil semua data user
-        $user = UserModel::where('username', 'manager')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
+        //dd($user);
 
         // Tampilkan ke view
         return view('user', ['data' => $user]); 
