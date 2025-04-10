86c35d4fadaae74c3c98b62da4fd36228ebbe758
Devita Dwi Lestari <devitadwilestari2@gmail.com>
2025-04-10 18:53:53 +0700

    js 5 Implementasi Filtering

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -20,7 +20,22 @@
             {{ session('error') }}
           </div> 
         @endif 
-      
+        <div class="row"> 
+            <div class="col-md-12"> 
+              <div class="form-group row"> 
+                <label class="col-1 control-label col-form-label">Filter:</label> 
+                <div class="col-3"> 
+                  <select class="form-control" id="level_id" name="level_id" required> 
+                    <option value="">- Semua -</option> 
+                    @foreach($level as $item) 
+                      <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option> 
+                    @endforeach 
+                  </select> 
+                  <small class="form-text text-muted">Level Pengguna</small> 
+                </div> 
+              </div> 
+            </div> 
+          </div>          
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user"> 
           <thead> 
             <tr>
@@ -32,7 +47,6 @@
             </tr> 
           </thead> 
         </table> 
-      
       </div>      
   </div>
 @endsection
@@ -48,7 +62,10 @@
         ajax: {
           url: "{{ url('user/list') }}",
           dataType: "json",
-          type: "POST"
+          type: "POST",
+          "data":function(d){
+            d.level_id = $('#level_id').val();
+          }
         },
         columns: [
           {
@@ -78,6 +95,9 @@
             searchable: false
           }
         ]
+      });
+      $('#level_id').on('change', function(){
+        dataUser.ajax.reload();
       });
     });
   </script>
